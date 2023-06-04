8a1bc0619288c4e4adf9e02fbade08d639d441f8
paulinagh <paulinanel50@gmail.com>
2023-06-04 16:56:20 +0700

    -

--- app/src/main/java/com/example/tfl/MainActivity.kt
+++ app/src/main/java/com/example/tfl/MainActivity.kt
@@ -4,20 +4,32 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.ExperimentalGetImage
 
+@ExperimentalGetImage
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
-        val buttonScan = findViewById<Button>(R.id.button_scan)
+        val buttonScanOpenCVRealTime = findViewById<Button>(R.id.button_scan_opencv_real_time)
+        val buttonScanCameraXRealTime = findViewById<Button>(R.id.button_scan_camerax_real_time)
 
-        buttonScan.setOnClickListener{
+        buttonScanOpenCVRealTime.setOnClickListener {
             startActivity(
                 Intent(
                     this@MainActivity,
-                    CameraActivity::class.java
+                    CameraOpenCVRealTimeActivity::class.java
+                ).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            )
+        }
+
+        buttonScanCameraXRealTime.setOnClickListener {
+            startActivity(
+                Intent(
+                    this@MainActivity,
+                    CameraCameraXRealTimeActivity::class.java
                 ).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
             )
         }
