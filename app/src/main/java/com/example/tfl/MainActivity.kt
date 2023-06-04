4faa2329efccf2e80e38f693d4c6ed7f9323e50e
paulinagh <paulinanel50@gmail.com>
2023-06-04 12:00:15 +0700

    -

--- app/src/main/java/com/example/tfl/MainActivity.kt
+++ app/src/main/java/com/example/tfl/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.example.tfl
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         setContentView(R.layout.activity_main)
+
+        val buttonScan = findViewById<Button>(R.id.button_scan)
+
+        buttonScan.setOnClickListener{
+            startActivity(
+                Intent(
+                    this@MainActivity,
+                    CameraActivity::class.java
+                ).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            )
+        }
     }
 }
